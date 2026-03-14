88948e4fc82815b1e470605f58f553e3fff65451
Виктория Кузовлева <viktoriakuzovleva@MacBook-Air-Viktoria.local>
2026-03-14 20:28:46 +0300

    Add tests

---
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -0,0 +1,171 @@
+package ru.yandex.practicum.filmorate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.controller.FilmController;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilmControllerTest {
+
+    private FilmController filmController;
+
+    @BeforeEach
+    void setUp() {
+        filmController = new FilmController();
+    }
+
+    @Test
+    void shouldCreateValidFilm() {
+        Film film = new Film();
+        film.setName("Film");
+        film.setDescription("Good film");
+        film.setReleaseDate(LocalDate.of(2000, 1, 1));
+        film.setDuration(100);
+
+        Film createdFilm = filmController.create(film);
+
+        assertNotNull(createdFilm.getId());
+        assertEquals("Film", createdFilm.getName());
+        assertEquals("Good film", createdFilm.getDescription());
+        assertEquals(LocalDate.of(2000, 1, 1), createdFilm.getReleaseDate());
+        assertEquals(100, createdFilm.getDuration());
+        assertEquals(1, filmController.findAll().size());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenFilmNameIsBlank() {
+        Film film = new Film();
+        film.setName("");
+        film.setDescription("Good film");
+        film.setReleaseDate(LocalDate.of(2000, 1, 1));
+        film.setDuration(100);
+
+        assertThrows(ValidationException.class, () -> filmController.create(film));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenDescriptionIsTooLong() {
+        Film film = new Film();
+        film.setName("Film");
+        film.setDescription("a".repeat(201));
+        film.setReleaseDate(LocalDate.of(2000, 1, 1));
+        film.setDuration(100);
+
+        assertThrows(ValidationException.class, () -> filmController.create(film));
+    }
+
+    @Test
+    void shouldCreateFilmWithDescriptionLength200() {
+        Film film = new Film();
+        film.setName("Film");
+        film.setDescription("a".repeat(200));
+        film.setReleaseDate(LocalDate.of(2000, 1, 1));
+        film.setDuration(100);
+
+        Film createdFilm = filmController.create(film);
+
+        assertNotNull(createdFilm.getId());
+        assertEquals(200, createdFilm.getDescription().length());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenReleaseDateIsTooEarly() {
+        Film film = new Film();
+        film.setName("Film");
+        film.setDescription("Good film");
+        film.setReleaseDate(LocalDate.of(1895, 12, 27));
+        film.setDuration(100);
+
+        assertThrows(ValidationException.class, () -> filmController.create(film));
+    }
+
+    @Test
+    void shouldCreateFilmWithReleaseDateAtBoundary() {
+        Film film = new Film();
+        film.setName("Film");
+        film.setDescription("Good film");
+        film.setReleaseDate(LocalDate.of(1895, 12, 28));
+        film.setDuration(100);
+
+        Film createdFilm = filmController.create(film);
+
+        assertNotNull(createdFilm.getId());
+        assertEquals(LocalDate.of(1895, 12, 28), createdFilm.getReleaseDate());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenDurationIsZero() {
+        Film film = new Film();
+        film.setName("Film");
+        film.setDescription("Good film");
+        film.setReleaseDate(LocalDate.of(2000, 1, 1));
+        film.setDuration(0);
+
+        assertThrows(ValidationException.class, () -> filmController.create(film));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenDurationIsNegative() {
+        Film film = new Film();
+        film.setName("Film");
+        film.setDescription("Good film");
+        film.setReleaseDate(LocalDate.of(2000, 1, 1));
+        film.setDuration(-1);
+
+        assertThrows(ValidationException.class, () -> filmController.create(film));
+    }
+
+    @Test
+    void shouldUpdateExistingFilm() {
+        Film film = new Film();
+        film.setName("Film");
+        film.setDescription("Good film");
+        film.setReleaseDate(LocalDate.of(2000, 1, 1));
+        film.setDuration(100);
+
+        Film createdFilm = filmController.create(film);
+
+        Film updatedFilm = new Film();
+        updatedFilm.setId(createdFilm.getId());
+        updatedFilm.setName("Updated film");
+        updatedFilm.setDescription("Updated description");
+        updatedFilm.setReleaseDate(LocalDate.of(2001, 1, 1));
+        updatedFilm.setDuration(120);
+
+        Film result = filmController.update(updatedFilm);
+
+        assertEquals(createdFilm.getId(), result.getId());
+        assertEquals("Updated film", result.getName());
+        assertEquals("Updated description", result.getDescription());
+        assertEquals(LocalDate.of(2001, 1, 1), result.getReleaseDate());
+        assertEquals(120, result.getDuration());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenUpdatingFilmWithoutId() {
+        Film film = new Film();
+        film.setName("Film");
+        film.setDescription("Good film");
+        film.setReleaseDate(LocalDate.of(2000, 1, 1));
+        film.setDuration(100);
+
+        assertThrows(ValidationException.class, () -> filmController.update(film));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenUpdatingNonExistingFilm() {
+        Film film = new Film();
+        film.setId(999L);
+        film.setName("Film");
+        film.setDescription("Good film");
+        film.setReleaseDate(LocalDate.of(2000, 1, 1));
+        film.setDuration(100);
+
+        assertThrows(ValidationException.class, () -> filmController.update(film));
+    }
+}
---
+++ src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
@@ -0,0 +1,180 @@
+package ru.yandex.practicum.filmorate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.controller.UserController;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    private UserController userController;
+
+    @BeforeEach
+    void setUp() {
+        userController = new UserController();
+    }
+
+    @Test
+    void shouldCreateValidUser() {
+        User user = new User();
+        user.setEmail("mail@mail.ru");
+        user.setLogin("dolore");
+        user.setName("Nick Name");
+        user.setBirthday(LocalDate.of(1946, 8, 20));
+
+        User createdUser = userController.create(user);
+
+        assertNotNull(createdUser.getId());
+        assertEquals("mail@mail.ru", createdUser.getEmail());
+        assertEquals("dolore", createdUser.getLogin());
+        assertEquals("Nick Name", createdUser.getName());
+        assertEquals(LocalDate.of(1946, 8, 20), createdUser.getBirthday());
+        assertEquals(1, userController.findAll().size());
+    }
+
+    @Test
+    void shouldSetLoginAsNameWhenNameIsBlank() {
+        User user = new User();
+        user.setEmail("mail@mail.ru");
+        user.setLogin("dolore");
+        user.setName("");
+        user.setBirthday(LocalDate.of(1946, 8, 20));
+
+        User createdUser = userController.create(user);
+
+        assertEquals("dolore", createdUser.getName());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenEmailIsNull() {
+        User user = new User();
+        user.setEmail(null);
+        user.setLogin("dolore");
+        user.setName("Nick Name");
+        user.setBirthday(LocalDate.of(1946, 8, 20));
+
+        assertThrows(ValidationException.class, () -> userController.create(user));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenEmailIsBlank() {
+        User user = new User();
+        user.setEmail("");
+        user.setLogin("dolore");
+        user.setName("Nick Name");
+        user.setBirthday(LocalDate.of(1946, 8, 20));
+
+        assertThrows(ValidationException.class, () -> userController.create(user));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenEmailHasNoAtSign() {
+        User user = new User();
+        user.setEmail("mailmail.ru");
+        user.setLogin("dolore");
+        user.setName("Nick Name");
+        user.setBirthday(LocalDate.of(1946, 8, 20));
+
+        assertThrows(ValidationException.class, () -> userController.create(user));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenLoginIsBlank() {
+        User user = new User();
+        user.setEmail("mail@mail.ru");
+        user.setLogin("");
+        user.setName("Nick Name");
+        user.setBirthday(LocalDate.of(1946, 8, 20));
+
+        assertThrows(ValidationException.class, () -> userController.create(user));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenLoginContainsSpaces() {
+        User user = new User();
+        user.setEmail("mail@mail.ru");
+        user.setLogin("my login");
+        user.setName("Nick Name");
+        user.setBirthday(LocalDate.of(1946, 8, 20));
+
+        assertThrows(ValidationException.class, () -> userController.create(user));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenBirthdayIsInFuture() {
+        User user = new User();
+        user.setEmail("mail@mail.ru");
+        user.setLogin("dolore");
+        user.setName("Nick Name");
+        user.setBirthday(LocalDate.now().plusDays(1));
+
+        assertThrows(ValidationException.class, () -> userController.create(user));
+    }
+
+    @Test
+    void shouldCreateUserWhenBirthdayIsToday() {
+        User user = new User();
+        user.setEmail("mail@mail.ru");
+        user.setLogin("dolore");
+        user.setName("Nick Name");
+        user.setBirthday(LocalDate.now());
+
+        User createdUser = userController.create(user);
+
+        assertNotNull(createdUser.getId());
+        assertEquals(LocalDate.now(), createdUser.getBirthday());
+    }
+
+    @Test
+    void shouldUpdateExistingUser() {
+        User user = new User();
+        user.setEmail("mail@mail.ru");
+        user.setLogin("dolore");
+        user.setName("Nick Name");
+        user.setBirthday(LocalDate.of(1946, 8, 20));
+
+        User createdUser = userController.create(user);
+
+        User updatedUser = new User();
+        updatedUser.setId(createdUser.getId());
+        updatedUser.setEmail("new@mail.ru");
+        updatedUser.setLogin("newlogin");
+        updatedUser.setName("New Name");
+        updatedUser.setBirthday(LocalDate.of(2000, 1, 1));
+
+        User result = userController.update(updatedUser);
+
+        assertEquals(createdUser.getId(), result.getId());
+        assertEquals("new@mail.ru", result.getEmail());
+        assertEquals("newlogin", result.getLogin());
+        assertEquals("New Name", result.getName());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenUpdatingUserWithoutId() {
+        User user = new User();
+        user.setEmail("mail@mail.ru");
+        user.setLogin("dolore");
+        user.setName("Nick Name");
+        user.setBirthday(LocalDate.of(1946, 8, 20));
+
+        assertThrows(ValidationException.class, () -> userController.update(user));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenUpdatingNonExistingUser() {
+        User user = new User();
+        user.setId(999L);
+        user.setEmail("mail@mail.ru");
+        user.setLogin("dolore");
+        user.setName("Nick Name");
+        user.setBirthday(LocalDate.of(1946, 8, 20));
+
+        assertThrows(ValidationException.class, () -> userController.update(user));
+    }
+}
